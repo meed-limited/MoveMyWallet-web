db1aa9797e5c7b61c46b6688aad10e984d060662
Pedrojok01 <pedrojok@pm.me>
2022-04-15 07:57:21 +0700

    Font issue fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import './fonts/LeonSans/LeonSans-Medium.otf';
+import './assets/fonts/LeonSans-Medium.otf';
 
 ReactDOM.render(
   <React.StrictMode>
